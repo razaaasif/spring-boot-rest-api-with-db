84e5fd316f871c8447fda9d35bde9ec3765f6e47
razaasif <aasifraza9123@gmail.com>
2023-07-20 17:22:15 +0530

    haha

---
+++ src/main/java/com/example/SpringBootRestApiDbApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiDbApplication.class, args);
+	}
+
+}
